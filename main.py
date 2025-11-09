dfdef502124c266ec45969af19fabc2922b99514
CHUIZ OSORIO JUAN DAVID <202214036@uns.edu.pe>
2025-11-09 18:53:10 -0500

    Actualizacion de definicion de version python

--- main.py
+++ main.py
@@ -57,7 +57,3 @@ async def predecir_temperatura(input_data: InputData):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
