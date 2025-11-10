70fb12f4b2d2895e8ae911d064c5fe03f0fdb9b1
CHUIZ OSORIO JUAN DAVID <202214036@uns.edu.pe>
2025-11-09 20:15:02 -0500

    base

--- main.py
+++ main.py
@@ -57,3 +57,7 @@ async def predecir_temperatura(input_data: InputData):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
